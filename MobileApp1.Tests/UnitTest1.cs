21741223221b292ca696bdf9977143db360f902b
Saba Gotsiridze <Gotsiridze.S@outlook.com>
2021-10-14 22:41:47 +0400

    add few unit tests inside assertion scope

--- MobileApp1.Tests/UnitTest1.cs
+++ MobileApp1.Tests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using FluentAssertions.Execution;
+using MobileApp1.ViewModels;
 using NUnit.Framework;
 
 namespace MobileApp1.Tests
@@ -13,6 +16,21 @@ namespace MobileApp1.Tests
         public void Test1()
         {
             Assert.Pass();
+        }
+
+        [Test]
+        public void AboutPageCtorWorks()
+        {
+            var aboutViewModel = new AboutViewModel();
+            //fluent assertion
+            using (new AssertionScope())
+            {
+                aboutViewModel.Title.Should().Be("About");
+                aboutViewModel.Title.Length.Should().Be(5);
+                aboutViewModel.Title.Should().BeEquivalentTo("about");
+
+            }
+
         }
     }
 }
